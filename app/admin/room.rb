b6418383a63acb7564570fc6a4ca9de0366ac482
Adam Zima <zimekx@gmail.com>
2016-05-22 18:55:19 +0200

    Add admin pages for Room & Message.

---
+++ app/admin/room.rb
@@ -0,0 +1,30 @@
+ActiveAdmin.register Room do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+  index do
+    id_column
+    column :name
+    actions
+  end
+
+  form do |f|
+    f.inputs 'Room Details' do
+      f.input :name
+    end
+    f.actions
+  end
+
+
+end
